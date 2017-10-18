9415d3816f49777510c97eace8a3c861c75728d2
Dong Nguyen <ndaidong@gmail.com>
2017-10-18 10:56:17 +0700

    Add ltvb service

--- adapters/ltvb.py
+++ adapters/ltvb.py
@@ -1,6 +1,10 @@
 # stdlib
 from datetime import datetime
 from json import dumps
+# lxml
+from lxml.builder import E as e
+from lxml.etree import tostring
+
 
 # Zato
 from zato.server.service import Service
@@ -17,16 +21,16 @@ class LTVBTransformer(Service):
 
     self.logger.info(type(root))
 
+    request = tostring(root)
+
     #msgHeader = root.Envelope.Header.MessageHeader
     #self.logger.info(type(msgHeader))
     #self.logger.info(root.From.OrganName.text)
     #self.logger.info(root.From.OrganizationInCharge.text)
 
-    payload = {}
-    #payload['id'] = 1
-
     params = {}
+    params['data'] = tostring(root);
     headers = {'Content-Type': 'application/json'}
-    response = ltvbService.conn.post(self.cid, dumps(payload), params, headers=headers)
+    response = ltvbService.conn.post(self.cid, dumps(payload), params, headers)
 
-    self.response.payload = payload
+    self.response.payload = response.text
